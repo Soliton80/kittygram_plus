f5094c6e0f6a8ee94bf575d7c45f669668228148
Kirill Sukharev <kirill@suharev.ru>
2022-04-10 21:37:28 +0300

    Authtoken

--- kittygram_plus/settings.py
+++ kittygram_plus/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'cats.apps.CatsConfig',
 ]
 
@@ -101,6 +102,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
